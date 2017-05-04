072294be0f38be6a7af4e713980fed972258ee8e
Ramiro Antonio <rantonio@dominiolidi.info.unlp.edu.ar>
2017-05-04 17:43:32 -0300

    Cambios en la deteccion de la piel

--- FaceInfo/faceinfo.py
+++ FaceInfo/faceinfo.py
@@ -3,6 +3,25 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
+def skin_extraction(img, roi):
+	hist_skin = cv2.calcHist([img],[0, 1],None,[12, 12],[0,180,0,256])
+	
+	cv2.normalize(hist_skin, hist_skin, 0, 255, cv2.NORM_MINMAX)
+	dst = cv2.calcBackProject([roi], [0,1], hist_skin, [0,180,0,256],1)
+	
+	disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+	cv2.filter2D(dst,-1,disc, dst)
+	
+	ret,thresh = cv2.threshold(dst,254,255,cv2.THRESH_BINARY)
+	thresh = cv2.merge((thresh,thresh,thresh))
+	
+	return cv2.bitwise_and(roi, thresh)
+
+def get_possible_hands(gray):
+	_, contours, _ = cv2.findContours(gray, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_TC89_L1)
+	contours = sorted(contours, key = cv2.contourArea ,reverse=True)
+	return contours[:2]
+	
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
 	help="path to the input image")
@@ -32,37 +51,25 @@ if len(faces) > 0:
 	crop_face = hsv[y_face:y_face+h_face, x_face:x_face+w_face]
 	
 	removal = hsv.copy()
-	removal[y_face:y_face+h_face, x_face:x_face+w_face] = 0
+	removal[y_face:y_face+int(h_face*1.2), x_face:x_face+w_face] = 0
 	
 	roi = removal[y_roi:y_roi+h_roi, x_roi:x_roi+w_roi]
 	
-	mask = np.zeros(crop_face.shape[:2],np.uint8)
-	bgdModel = np.zeros((1,65),np.float64)
-	fgdModel = np.zeros((1,65),np.float64)
-	rect = (int(w_face/2),int(h_face/2),int(w_face/4),int(h_face/4))
-	cv2.grabCut(crop_face,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
-	mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
-	crop_face = crop_face*mask2[:,:,np.newaxis]
+	res = skin_extraction(crop_face, roi)
 	
-	cv2.imshow('Face',crop_face)
-	cv2.waitKey(0)
+	#kernel = np.ones((5,5),np.uint8)
+	#res = cv2.morphologyEx(res, cv2.MORPH_OPEN, kernel)
+	#res = cv2.erode(res,kernel,iterations = 1)
+	#res = cv2.morphologyEx(res, cv2.MORPH_CLOSE, kernel)
+	#res = cv2.dilate(res,kernel,iterations = 1)
 	
-	hist_skin = cv2.calcHist([crop_face],[0, 1],None,[180, 256],[0,180,0,256])
-	
-	cv2.normalize(hist_skin, hist_skin, 0, 255, cv2.NORM_MINMAX)
-	dst = cv2.calcBackProject([roi], [0,1], hist_skin, [0,180,0,256],1)
-	
-	disc = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-	cv2.filter2D(dst,-1,disc, dst)
-	
-	ret,thresh = cv2.threshold(dst,50,255,cv2.THRESH_BINARY)
-	thresh = cv2.merge((thresh,thresh,thresh))
-	
-	res = cv2.bitwise_and(roi, thresh)
-	
-	#res = np.vstack((roi, thresh, res))
+	h,s,v = cv2.split(res)
+	hands = get_possible_hands(v)
 	
 	res =cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
+	
+	cv2.drawContours(res, hands, -1, (0,255,0), 2)
+	
 	cv2.imshow('Result',res)
 else:
 	print("No fueron encontradas caras")
